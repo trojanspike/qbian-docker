c05d86df37da0cb8d096073a605645e6ad3e5702
trojanspike <v3047-github@yahoo.co.uk>
2016-02-08 14:11:09 +0000

    qbian-docker inject files

---
+++ injects/qbian-docker/etc/skel/init.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+COMMAND=$1 # if [$COMMD = start | stop | reload]
+
+WORK_DIR=~/
+PORT=8080
+
+ENV="develop"
+
+eth0=$(cat /sys/class/net/eth0/operstate); # up | down
+wlan0=$(cat /sys/class/net/wlan0/operstate); # up | down
+
+###
+
+if [ "$COMMAND" == "start" ];then
+	echo "Starting docker ui"
+	# docker run -d docker_ui
+fi
+
+if [ "$COMMAND" == "reload" ];then
+	echo "reload" # cd ~/Server && npm run service-reload
+fi
+
+if [ "$COMMAND" == "stop" ];then
+	echo "stop" # cd ~/Server && npm run service-stop
+fi
