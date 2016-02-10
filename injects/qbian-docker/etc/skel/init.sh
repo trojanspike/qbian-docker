0a6345981f4206c3822ba88f0bac68a1ff6876e9
trojanspike <v3047-github@yahoo.co.uk>
2016-02-10 10:15:53 +0000

    start docker after 8 secs

--- injects/qbian-docker/etc/skel/init.sh
+++ injects/qbian-docker/etc/skel/init.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 COMMAND=$1 # if [$COMMD = start | stop | reload]
 
-WORK_DIR=~/
-PORT=8080
-
 ENV="develop"
 
 eth0=$(cat /sys/class/net/eth0/operstate); # up | down
@@ -13,7 +10,7 @@ wlan0=$(cat /sys/class/net/wlan0/operstate); # up | down
 
 if [ "$COMMAND" == "start" ];then
 	echo "Starting docker ui"
-	docker start qbian-docker-ui-init
+	sleep 8; docker start qbian-docker-ui-init
 fi
 
 if [ "$COMMAND" == "reload" ];then
